0eeb945ee97d58a86411386c3b751c73083801c9
Yujin <yujinjcho@gmail.com>
2016-12-11 12:41:02 -0800

    Add unit tests for remaining methods

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
+import os
 import unittest
 import rsa_sign
 import json
 from OpenSSL import crypto
 
 class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # setup for create_identifier
+        cls.create_identifier_filename = 'test_create.pem'
 
-    def test_create_key_pair(self):
-        key_pair = rsa_sign._create_key_pair_object()
-        self.assertTrue(isinstance(key_pair, crypto.PKey))
-        self.assertTrue(key_pair.check())
+        # setup for retrieve tests
+        cls.retrieve_file = 'test_private.pem'
+        cls.retrieve_identifier_message = 'testing retrieve identifiers'
+        rsa_sign._create_identifiers(
+            cls.retrieve_identifier_message,
+            cls.retrieve_file
+        )
 
-    def test_create_signature(self):
-        key_pair = rsa_sign._create_key_pair_object()
-        message = 'testing create signature'
-        signature = rsa_sign._create_signature(key_pair, message)
-        self.assertTrue(isinstance(signature, basestring))
+        # for testing create_file
+        cls.create_filename = 'new_file'
 
-    def test_create_certificate(self):
-        key_pair = rsa_sign._create_key_pair_object()
-        cert = rsa_sign._create_certificate(key_pair)
-        self.assertTrue(isinstance(cert, crypto.X509))
+    def setUp(self):
+        self.key_pair = rsa_sign._create_key_pair_object()
 
     def test_signed_identifier(self):
         message = 'this is a test'
@@ -32,11 +35,67 @@ class Test(unittest.TestCase):
         self.assertTrue('signature' in result)
         self.assertEqual(result['pubkey'][0:21], '-----BEGIN PUBLIC KEY')
 
+    def test_create_identifiers(self):
+        message = 'testing create identifiers'
+        self.assertFalse(
+            os.path.exists(self.create_identifier_filename)
+        )
+        rsa_sign._create_identifiers(
+            message, self.create_identifier_filename
+        )
+        self.assertTrue(os.path.exists(self.create_identifier_filename))
+
+    def test_retrieve_identifiers(self):
+        self.assertTrue(os.path.exists(self.retrieve_file))
+        response = rsa_sign._retrieve_identifiers(
+            self.retrieve_identifier_message,
+            self.retrieve_file
+        )
+        self.assertTrue(isinstance(response, dict))
+
+    def test_retrieve_key_pair(self):
+        key_pair = rsa_sign._retrieve_key_pair(self.retrieve_file)
+        self.assertTrue(isinstance(key_pair, crypto.PKey))
+        self.assertEqual(
+            crypto.dump_privatekey(crypto.FILETYPE_PEM, key_pair)[0:22],
+            '-----BEGIN PRIVATE KEY'
+        )
+
+    def test_create_file(self):
+        message = "test create file"
+        self.assertFalse(os.path.exists(self.create_filename))
+        rsa_sign._create_file(self.create_filename, message)
+        self.assertTrue(os.path.exists(self.create_filename))
+        saved_message = open(self.create_filename).read()
+        self.assertEqual(saved_message, message)
+
+    def test_create_key_pair(self):
+        key_pair = rsa_sign._create_key_pair_object()
+        self.assertTrue(isinstance(key_pair, crypto.PKey))
+        self.assertTrue(key_pair.check())
+
+
+    def test_create_certificate(self):
+        cert = rsa_sign._create_certificate(self.key_pair)
+        self.assertTrue(isinstance(cert, crypto.X509))
+
+    def test_create_signature(self):
+        message = 'testing create signature'
+        signature = rsa_sign._create_signature(self.key_pair, message)
+        self.assertTrue(isinstance(signature, basestring))
+
+    def test_format_response(self):
+        message = 'test format response'
+        signature = rsa_sign._create_signature(self.key_pair, message)
+        response = rsa_sign._format_response(message, signature, self.key_pair)
+        self.assertTrue('message' in response)
+        self.assertTrue('signature' in response)
+        self.assertTrue('pubkey' in response)
+
     def test_signature_verification(self):
         message = 'signature verification test'
-        key_pair = rsa_sign._create_key_pair_object()
-        signature = rsa_sign._create_signature(key_pair, message)
-        cert = rsa_sign._create_certificate(key_pair)
+        signature = rsa_sign._create_signature(self.key_pair, message)
+        cert = rsa_sign._create_certificate(self.key_pair)
 
         try:
             crypto.verify(cert, signature, message, 'sha256')
@@ -50,6 +109,12 @@ class Test(unittest.TestCase):
             self.assertTrue(False)
         except Exception as e:
             self.assertTrue(True)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.create_identifier_filename)
+        os.remove(cls.retrieve_file)
+        os.remove(cls.create_filename)
 
 if __name__ == '__main__':
     unittest.main()
